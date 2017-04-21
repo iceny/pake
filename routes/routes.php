57a179d59a33b4bc3d6a84144cb1e9aee9a6ab1e
voocel <voocel@163.com>
2017-04-21 22:19:37 +0800

    添加

--- routes/routes.php
+++ routes/routes.php
@@ -4,7 +4,7 @@ use NoahBuscher\Macaw\Macaw;
 Macaw::get('/', function() {
   echo "成功了！";
 });
-//匹配路由
+//匹配路由自定义规则
 Macaw::get('/home', 'HomeController@home');
 // Macaw::get('(:all)', function($fu) {
 //   echo '未匹配到路由<br>'.$fu;

72abb9f506967d8a8a1b502018713e4a83d44efd
voocel <voocel@gmail.com>
2017-04-22 23:45:56 +0800

    ok

--- routes/routes.php
+++ routes/routes.php
@@ -9,6 +9,7 @@ Macaw::get('/home', 'HomeController@home');
 // Macaw::get('(:all)', function($fu) {
 //   echo '未匹配到路由<br>'.$fu;
 // });
-Macaw::get('/admin','AdminController@index');
+
+Macaw::get('/admin','AdminController@index');      //后台首页
 Macaw::get('/article','ArticleController@index');
 Macaw::dispatch();

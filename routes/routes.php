c2f855fe8a22004d0e716a5c438872e8b23fdb8d
voocel <voocel@gmail.com>
2017-04-28 23:58:14 +0800

    ok

--- routes/routes.php
+++ routes/routes.php
@@ -4,7 +4,7 @@ use NoahBuscher\Macaw\Macaw;
 Macaw::get('/', function() {
   echo "成功了！";
 });
-//匹配路由自定义规则了09999
+//匹配路由自定义规则了099999
 Macaw::get('/home', 'HomeController@home');
 // Macaw::get('(:all)', function($fu) {
 //   echo '未匹配到路由<br>'.$fu;
